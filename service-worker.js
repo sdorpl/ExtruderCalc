219d9431b60b3888bb3af922791f5c8bfa423bdc
sdorpl <macszymk@outlook.com>
2019-04-20 17:11:06 +0200

    change version

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var cacheName = 'excalc 0.4.4';
+var cacheName = 'excalc 0.4.5';
 var filesToCache = [
   '/',
   '/index.html',
