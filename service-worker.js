a9bb6ac7981faf1b1b4849b6863b39a3990e37ac
sdorpl <macszymk@outlook.com>
2020-06-11 15:19:12 +0200

    updt

--- service-worker.js
+++ service-worker.js
@@ -24,7 +24,7 @@ workbox.googleAnalytics.initialize();
 
 // CODELAB: Update cache names any time any of the cached files change.
 
-const CACHE_NAME = 'excalc-main-20.06.01';
+const CACHE_NAME = 'excalc-main-20.06.02';
 
 
 // CODELAB: Add list of files to cache here.
